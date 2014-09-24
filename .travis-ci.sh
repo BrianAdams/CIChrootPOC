64bf9472301eb202e1c9b52990212ac50d03d252
badevguru <brian@babastik.com>
2014-09-24 13:25:29 -0700

    added a depends

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -11,7 +11,7 @@ CHROOT_ARCH=armhf
 HOST_DEPENDENCIES="debootstrap qemu-user-static binfmt-support sbuild"
 
 # Debian package dependencies for the chrooted environment
-GUEST_DEPENDENCIES=""
+GUEST_DEPENDENCIES="pv"
 
 # Command used to run the tests
 TEST_COMMAND="uname -a"
